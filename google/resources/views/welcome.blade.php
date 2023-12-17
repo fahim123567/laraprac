1a5cf0fceda10237ee4b82cf4ba9d7bb84f1455b
fahim123567 <mdreazuddinchowdhury@gmail.com>
2023-12-18 00:05:59 +0600

    Your commit message here

---
+++ google/resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+
+        <!-- Fonts -->
+        <link
+            rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"
+        />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css'])
+    </head>
+    <body class="antialiased font-sans bg-gray-100 text-gray-900 dark:bg-dark-eval-0 dark:text-gray-200">
+        <div class="relative flex items-top justify-center min-h-screen sm:items-center py-4 sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-400 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-400 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-400 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div>
+                <h1 align="justify-center">Welcome to InkByte</h1>
+                <p align="justify-center">"Where Words Unite, Ideas Ignite"</p>
+            </div>
+    </body>
+</html>
